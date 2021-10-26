3bc52483bf5cdf3bdb3f8a8620fdea37c33f3b97
JonattasJK <jonataseliask@hotmail.com>
2021-10-26 14:41:25 -0300

    feat: started the video robot

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const robots = {
     userInput: require(path.join(__dirname, 'robots', 'user-input.js')),
     text: require(path.join(__dirname, 'robots', 'text.js')),
     image: require(path.join(__dirname, 'robots', 'image.js')),
-    state: require(path.join(__dirname, 'robots', 'state.js'))
+    state: require(path.join(__dirname, 'robots', 'state.js')),
+    video: require(path.join(__dirname, 'robots', 'video.js'))
 }
 
 async function start() {
 
-    robots.userInput()
-    await robots.text()
-    await robots.image()
+    //robots.userInput()
+    //await robots.text()
+    //await robots.image()
+    await robots.video()
 
     const content = robots.state.load()
-    //console.dir(content, { depth: null })
+    console.dir(content, { depth: null })
 }
 
 start()
